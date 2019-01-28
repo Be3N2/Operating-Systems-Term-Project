fdb294b2d64e3bb2c643e5185177e8c9b058343d
Be3N2 <2017WakefieldB@gmail.com>
2019-01-28 11:21:48 -0500

    Read in arguments

--- home.cpp
+++ home.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	cout << "Hello WOrld" << endl;
+
+	//This is an example/test file for how to read in program arguments
+
+
+
+	//Hello to the first passed argument
+	cout << "Welcome " << argv[1] << endl;
+		
+	// ===================================
+	// How to loop through all the arguments
+
+	for (int i = 0; i < argc; ++i) 
+        cout << argv[i] << "\n"; 
+
+    //Read in a character to keep the command window open (only necessary if clicking on built exe file)
+	cout << "Press any button to exit the program" << endl;
+	char c;
+	cin >> c;
 }
