8ff0ec81aa6a25a2307c6300f40a1abecc8682d8
Be3N2 <2017WakefieldB@gmail.com>
2019-04-12 11:53:57 -0400

    updating changes before class

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 //details on pack code here https://gcc.gnu.org/onlinedocs/gcc/Structure-Layout-Pragmas.html
-#pragma pack(push, 1)
 // __operator__ ((packed))
+#pragma pack(push, 1)
 struct headerDescriptor {
     char preheader[64];
     char imageSignature[4];
@@ -51,51 +51,51 @@ struct secondDescriptor {
 };
 #pragma pack(pop)
 
-headerDescriptor descriptor1;
-headerDescriptor& ref = descriptor1;
-secondDescriptor descriptor2;
-secondDescriptor& refToDescriptor2 = descriptor2;
 
 //open Takes file name, returns pointer to second struct
-void openFile(char filename[], secondDescriptor &refToDescriptor2);
-//void closeFile(secondDescriptor * refToDescriptor2);
+void openFile(char filename[], secondDescriptor &descriptor2);
+//void closeFile(secondDescriptor * descriptor2);
 //read
 //seek
 //close
 
 int main(int argc, char *argv[])
 {
-    openFile(argv[1], refToDescriptor2);
-    cout << "SIZE OF " << sizeof(refToDescriptor2.hd);
-    for (int i = 0;i < sizeof(refToDescriptor2.hd.preheader); i++) {
-        cout << refToDescriptor2.hd.preheader[i];
+
+    headerDescriptor descriptor1;
+    secondDescriptor descriptor2;
+
+    openFile(argv[1], descriptor2);
+    cout << "SIZE OF " << sizeof(descriptor2.hd);
+    for (int i = 0;i < sizeof(descriptor2.hd.preheader); i++) {
+        cout << descriptor2.hd.preheader[i];
     }
-    for (int i = 0;i < sizeof(refToDescriptor2.hd.imageSignature); i++) {
-        cout << refToDescriptor2.hd.imageSignature[i];
+    for (int i = 0;i < sizeof(descriptor2.hd.imageSignature); i++) {
+        cout << descriptor2.hd.imageSignature[i];
     }
-    cout << "Version num: " << refToDescriptor2.hd.versionNum << endl;
-    cout << "Size of Header: " << refToDescriptor2.hd.sizeOfHeader << endl;
-    cout << "Image Type: " << refToDescriptor2.hd.imageType << endl;
-    cout << "image Flags: " << refToDescriptor2.hd.imageFlags << endl;
+    cout << "Version num: " << descriptor2.hd.versionNum << endl;
+    cout << "Size of Header: " << descriptor2.hd.sizeOfHeader << endl;
+    cout << "Image Type: " << descriptor2.hd.imageType << endl;
+    cout << "image Flags: " << descriptor2.hd.imageFlags << endl;
     cout << "image Description";
-    for (int i = 0;i < sizeof(refToDescriptor2.hd.imageDescription); i++) {
-        cout << refToDescriptor2.hd.imageDescription[i];
+    for (int i = 0;i < sizeof(descriptor2.hd.imageDescription); i++) {
+        cout << descriptor2.hd.imageDescription[i];
     }
-    cout << endl << "Offset blocks: " << refToDescriptor2.hd.offsetBlocks << endl;
-    cout << "offset Data: " << refToDescriptor2.hd.offsetData << endl;
-    cout << "num of cylinders: " << refToDescriptor2.hd.numOfCylinders << endl;
-    cout << "num of heads: " << refToDescriptor2.hd.numOfHeads << endl;
-    cout << "num of sectors: " << refToDescriptor2.hd.numOfSectors << endl;
-    cout << "Disk size: " << refToDescriptor2.hd.diskSize << endl;
-    cout << "Block size: " << refToDescriptor2.hd.blockSize << endl;
-    cout << "Sector size: " << refToDescriptor2.hd.sectorSize << endl;
-    //closeFile(refToDescriptor2);
+    cout << endl << "Offset blocks: " << descriptor2.hd.offsetBlocks << endl;
+    cout << "offset Data: " << descriptor2.hd.offsetData << endl;
+    cout << "num of cylinders: " << descriptor2.hd.numOfCylinders << endl;
+    cout << "num of heads: " << descriptor2.hd.numOfHeads << endl;
+    cout << "num of sectors: " << descriptor2.hd.numOfSectors << endl;
+    cout << "Disk size: " << descriptor2.hd.diskSize << endl;
+    cout << "Block size: " << descriptor2.hd.blockSize << endl;
+    cout << "Sector size: " << descriptor2.hd.sectorSize << endl;
+    //closeFile(descriptor2);
     return 0;
 }
 
 //C:\Users\2017W\Documents\compSci\Operating-Systems-Term-Project\VDITestFiles\Good\Test-fixed-1k.vdi
 
-void openFile(char filename[], secondDescriptor &refToDescriptor2) {
+void openFile(char filename[], secondDescriptor &descriptor2) {
     //file descriptor
     int fd;
 
@@ -110,23 +110,23 @@ void openFile(char filename[], secondDescriptor &refToDescriptor2) {
     else
     {
         //set the filed descriptor
-        refToDescriptor2.fd = fd;
+        descriptor2.fd = fd;
         
         //read the header descriptor
-        if (read(fd, &(refToDescriptor2.hd), sizeof(refToDescriptor2.hd)) < 0)
+        if (read(fd, &(descriptor2.hd), sizeof(descriptor2.hd)) < 0)
             cout << "ERROR"<< endl;
         
         //set cursor to 0
-        refToDescriptor2.cursor = 0;
+        descriptor2.cursor = 0;
 
         //return pointer
         //return null pointer if anything went wrong
     }
 }
 
-void closeFile(secondDescriptor * refToDescriptor2) {
+void closeFile(secondDescriptor * descriptor2) {
     //could close file too
-    delete refToDescriptor2;
+    delete descriptor2;
 }
 
 //https://stackoverflow.com/questions/32717269/how-to-read-an-integer-and-a-char-with-read-function-in-c
